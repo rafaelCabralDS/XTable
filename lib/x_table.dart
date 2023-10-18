2b2372394d89abf4e01ece4a591bd44e2123a1c1
Rafael <rafaelcabraldossantos@outlook.com>
2023-10-18 09:20:29 -0300

    0.0.3

--- lib/x_table.dart
+++ lib/x_table.dart
@@ -5,3 +5,4 @@ export 'src/table.dart';
 export 'src/header.dart' hide TableHeaderBuilder;
 export 'src/item.dart' hide TableItemBuilder;
 export 'src/theme.dart';
+export 'package:syncfusion_flutter_datagrid/datagrid.dart';
